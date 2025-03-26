1878bcc03145aa157122a20f05f13a0c9adc7e8b
kthurm <karalthurmond@gmail.com>
2025-03-26 14:16:27 -0400

    Adds logo

--- resources/js/Components/NavBar.vue
+++ resources/js/Components/NavBar.vue
@@ -1,42 +1,63 @@
 <script setup>
 import ApplicationLogo from '@/Components/ApplicationLogo.vue';
-import NavLink from './NavLink.vue';
+import NavLink from '@/Components/NavLink.vue';
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    canLogin: {
+        type: Boolean,
+        default: true, // Set a default if not provided
+    },
+    canRegister: {
+        type: Boolean,
+        default: true, // Set a default if not provided
+    },
+});
 </script>
 
 <template>
-    <nav class="w-screen bg-white shadow">
+    <div class="relative top-0 w-screen bg-white shadow-lg lg:sticky">
         <div
             class="mx-auto flex max-w-7xl items-center justify-between px-4 py-4"
         >
             <a href="/" class="navbar-brand">
-                <ApplicationLogo class="h-24 w-auto fill-current text-black" />
+                <ApplicationLogo
+                    class="h-9 w-auto fill-current text-black sm:h-16"
+                />
             </a>
-            <div>
+
+            <div class="flex items-center space-x-4 px-2">
                 <NavLink :href="route('books.index')">Books</NavLink>
-                <div v-if="canLogin" class="-mx-3 flex flex-1 justify-end">
-                    <template v-if="$page.props.auth.user">
-                        <Link :href="route('dashboard')">Dashboard</Link>
-                    </template>
+                <nav
+                    v-if="props.canLogin"
+                    class="-mx-3 flex flex-1 justify-end"
+                >
+                    <Link
+                        v-if="$page.props.auth.user"
+                        :href="route('dashboard')"
+                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                    >
+                        Dashboard
+                    </Link>
 
                     <template v-else>
-                        <Link :href="route('login')">Log in</Link>
+                        <Link
+                            :href="route('login')"
+                            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                        >
+                            Log in
+                        </Link>
 
                         <Link
-                            v-if="canRegister"
+                            v-if="props.canRegister"
                             :href="route('register')"
                             class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                         >
                             Register
                         </Link>
                     </template>
-                </div>
+                </nav>
             </div>
         </div>
-    </nav>
+    </div>
 </template>
-
-<script>
-export default {
-    name: 'NavBar',
-};
-</script>
