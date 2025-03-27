1f1a9b4e47272de8a08bf8a5a89d3fab6067490c
kthurm <karalthurmond@gmail.com>
2025-03-27 16:31:10 -0400

    Adds checkout page

---
+++ resources/js/Pages/Books/CheckedOut.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    checkedOutBooks: Array,
+    csrfToken: String,
+});
+</script>
+<template>
+    <div v-for="book in checkedOutBooks" :key="book.id">
+        <div class="book-details">
+            <h3>{{ book.title }}</h3>
+            <p>{{ book.author }}</p>
+            <p>{{ book.published_year }}</p>
+        </div>
+
+        <form :action="route('books.return', book.id)" method="POST">
+            <input type="hidden" name="_method" value="POST" />
+            <input type="hidden" name="_token" :value="csrfToken" />
+            <button type="submit" class="btn btn-danger">Return Book</button>
+        </form>
+    </div>
+</template>
