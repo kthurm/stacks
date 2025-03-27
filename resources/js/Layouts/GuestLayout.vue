afc0914dd067233560ca58c32ff9924945abbdf7
kthurm <karalthurmond@gmail.com>
2025-03-27 16:28:08 -0400

    Restyled removing nav bar

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import NavBar from '../Components/NavBar.vue';
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 </script>
 <template>
-    <NavBar />
     <div
-        class="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:pt-12"
+        class="radial-gradient flex min-h-screen flex-col items-center pt-6 sm:pt-12"
     >
+        <ApplicationLogo class="h-9 w-auto fill-current text-white sm:h-16" />
         <div
             class="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg"
         >
