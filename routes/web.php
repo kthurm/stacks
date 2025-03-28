b226b4561d6748b30f431581c5f202a0f7699093
kthurm <karalthurmond@gmail.com>
2025-03-28 10:55:38 -0400

    Removes duplicate routes

--- routes/web.php
+++ routes/web.php
@@ -17,17 +17,20 @@ Route::get('/', function () {
 Route::middleware(['auth'])->group(function () {
     Route::resource('books', BookController::class);
 
-    Route::get('/books/checkedout', [BookController::class, 'checkedOutBooks'])->name('books.checkedOut');
+    Route::get('/books/checkedout', [BookController::class, 'checkedOutBooks'])->name('books.checkedout');
     Route::post('/books/{book}/borrow', [BookController::class, 'borrow'])->name('books.borrow');
+
     Route::post('/books/return/{bookId}', [BookController::class, 'returnBook'])->name('books.return');
-    Route::get('/books/{book}/edit', [BookController::class, 'edit'])->name('books.edit');
-    Route::put('/books/{book}', [BookController::class, 'update'])->name('books.update');
-    Route::delete('/books/{book}', [BookController::class, 'destroy'])->name('books.destroy');
+
+    // Route::get('/books/{book}/edit', [BookController::class, 'edit'])->name('books.edit');
+    // Route::put('/books/{book}', [BookController::class, 'update'])->name('books.update');
+    // Route::delete('/books/{book}', [BookController::class, 'destroy'])->name('books.destroy');
 });
 
 Route::get('/dashboard', [BookController::class, 'dashboard'])
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
